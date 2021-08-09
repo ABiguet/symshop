9fba086589e0e8d9e5f4ccb46a6b746525210398
Aurelien Biguet <abiguet@mac-mini-de-aurelien-1.home>
2021-08-09 18:14:29 +0200

    cool

--- src/Controller/TestController.php
+++ src/Controller/TestController.php
@@ -17,7 +17,12 @@ class TestController
     }
 
     /**
-     * @Route ("/test/{age<\d+>?0}", name="test", methods={"GET", "POST"}, host="127.0.0.1", schemes={"https"})
+     * @Route (
+     *     "/test/{age<\d+>?0}",
+     *     name="test",
+     *     methods={"GET", "POST"},
+     *     host="127.0.0.1",
+     *     schemes={"https"})
      */
     public function test(Request $request, $age) : Response
     {
